d9a5ea9fc89aaa9cdd395327309356e9a22a598b
MING-XUAN XU <ja99181@gmail.com>
2024-11-20 21:06:44 +0800

    mechatronics launch

--- launch/mechatronics.launch.py
+++ launch/mechatronics.launch.py
@@ -1,16 +1,15 @@
 import os
 
 from launch import LaunchDescription
-from launch.actions.declare_launch_argument import DeclareLaunchArgument
-from launch.actions.include_launch_description import IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from ament_index_python import get_package_share_directory
+from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
 
-    # Include launch file
+    # Include other launch files
     tf_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(
@@ -37,25 +36,28 @@ def generate_launch_description():
         ),
     )
     imu_launch = IncludeLaunchDescription(
-        os.path.join(
-            get_package_share_directory("serial_imu"),
-            "launch",
-            "imu_pub.launch.py",
-        ),
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory("serial_imu"),
+                "launch",
+                "imu_pub.launch.py",
+            ),
+        )
     )
-    micro_ros_launch = IncludeLaunchDescription(
-        os.path.join(
-            get_package_share_directory("micro_ros_agent"),
-            "launch",
-            "micro_ros_agent_launch.py",
-        ),
+    micro_ros_node = Node(
+        package="micro_ros_agent",
+        executable="micro_ros_agent",
+        name="micro_ros_agent",
+        arguments=["serial", "--dev", "/dev/ttyACM0"],
+        output="screen",  # This ensures output is visible in the terminal
     )
 
-    ld = LaunchDescription()
-    ld.add_action(tf_launch)
-    ld.add_action(hokuyo_launch)
-    ld.add_action(merged_launch)
-    ld.add_action(imu_launch)
-    ld.add_action(micro_ros_launch)
-
-    return ld
+    return LaunchDescription(
+        [
+            tf_launch,
+            hokuyo_launch,
+            merged_launch,
+            imu_launch,
+            micro_ros_node,
+        ]
+    )
