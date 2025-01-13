345c6d03a67d35475e2a1925d0d31b0c0c65fa58
MING-XUAN XU <ja99181@gmail.com>
2025-01-13 19:37:27 +0800

    feat(nav2):

---
+++ launch/navigation_bringup_launch.py
@@ -0,0 +1,207 @@
+import os
+import subprocess
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    GroupAction,
+    IncludeLaunchDescription,
+    SetEnvironmentVariable,
+)
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition
+from launch_ros.actions import Node
+from launch_ros.actions import PushRosNamespace
+from launch_ros.descriptions import ParameterFile
+from nav2_common.launch import RewrittenYaml, ReplaceString
+
+
+def source_setup_files():
+    """Source the required setup files before running the launch file."""
+    setup_commands = [
+        "source ~/nav2_ws/install/setup.bash",
+        "source ~/ros2_ws/install/setup.bash",
+    ]
+    for command in setup_commands:
+        subprocess.run(command, shell=True, check=True, executable="/bin/bash")
+
+
+def generate_launch_description():
+    # Source setup files
+    source_setup_files()
+
+    # Get the launch directory
+    bringup_dir = get_package_share_directory("nav2_bringup")
+    scl_amr_dir = get_package_share_directory("scl_amr")
+    launch_dir = os.path.join(bringup_dir, "launch")
+    rviz_config_dir = os.path.join(scl_amr_dir, "rviz", "nav2_default.rviz")
+
+    # Create the launch configuration variables
+    namespace = LaunchConfiguration("namespace")
+    use_namespace = LaunchConfiguration("use_namespace")
+    map_yaml_file = LaunchConfiguration(
+        "map", default=os.path.join(scl_amr_dir, "map", "lio_2.yaml")
+    )
+    use_sim_time = LaunchConfiguration("use_sim_time", default="false")
+    params_file = LaunchConfiguration(
+        "params_file",
+        default=os.path.join(scl_amr_dir, "config", "nav2_params.yaml"),
+    )
+    autostart = LaunchConfiguration("autostart", default="true")
+    use_composition = LaunchConfiguration("use_composition", default="True")
+    use_respawn = LaunchConfiguration("use_respawn", default="False")
+    log_level = LaunchConfiguration("log_level", default="info")
+
+    # Map fully qualified names to relative ones so the node's namespace can be prepended.
+    remappings = [("/tf", "tf"), ("/tf_static", "tf_static")]
+
+    # Create our own temporary YAML files that include substitutions
+    param_substitutions = {"use_sim_time": use_sim_time, "yaml_filename": map_yaml_file}
+
+    params_file = ReplaceString(
+        source_file=params_file,
+        replacements={"<robot_namespace>": ("/", namespace)},
+        condition=IfCondition(use_namespace),
+    )
+
+    configured_params = ParameterFile(
+        RewrittenYaml(
+            source_file=params_file,
+            root_key=namespace,
+            param_rewrites=param_substitutions,
+            convert_types=True,
+        ),
+        allow_substs=True,
+    )
+
+    stdout_linebuf_envvar = SetEnvironmentVariable(
+        "RCUTILS_LOGGING_BUFFERED_STREAM", "1"
+    )
+
+    declare_namespace_cmd = DeclareLaunchArgument(
+        "namespace", default_value="", description="Top-level namespace"
+    )
+
+    declare_use_namespace_cmd = DeclareLaunchArgument(
+        "use_namespace",
+        default_value="false",
+        description="Whether to apply a namespace to the navigation stack",
+    )
+
+    declare_map_yaml_cmd = DeclareLaunchArgument(
+        "map",
+        default_value=map_yaml_file,
+        description="Full path to map yaml file to load",
+    )
+
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        "use_sim_time",
+        default_value="false",
+        description="Use simulation (Gazebo) clock if true",
+    )
+
+    declare_params_file_cmd = DeclareLaunchArgument(
+        "params_file",
+        default_value=params_file,
+        description="Full path to the ROS2 parameters file to use for all launched nodes",
+    )
+
+    declare_autostart_cmd = DeclareLaunchArgument(
+        "autostart",
+        default_value="true",
+        description="Automatically startup the nav2 stack",
+    )
+
+    declare_use_composition_cmd = DeclareLaunchArgument(
+        "use_composition",
+        default_value="True",
+        description="Whether to use composed bringup",
+    )
+
+    declare_use_respawn_cmd = DeclareLaunchArgument(
+        "use_respawn",
+        default_value="False",
+        description="Whether to respawn if a node crashes. Applied when composition is disabled.",
+    )
+
+    declare_log_level_cmd = DeclareLaunchArgument(
+        "log_level", default_value="info", description="log level"
+    )
+
+    # Specify the actions
+    bringup_cmd_group = GroupAction(
+        [
+            PushRosNamespace(condition=IfCondition(use_namespace), namespace=namespace),
+            Node(
+                condition=IfCondition(use_composition),
+                name="nav2_container",
+                package="rclcpp_components",
+                executable="component_container_isolated",
+                parameters=[configured_params, {"autostart": autostart}],
+                arguments=["--ros-args", "--log-level", log_level],
+                remappings=remappings,
+                output="screen",
+            ),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    os.path.join(launch_dir, "localization_launch.py")
+                ),
+                launch_arguments={
+                    "namespace": namespace,
+                    "map": map_yaml_file,
+                    "use_sim_time": use_sim_time,
+                    "autostart": autostart,
+                    "params_file": params_file,
+                    "use_composition": use_composition,
+                    "use_respawn": use_respawn,
+                    "container_name": "nav2_container",
+                }.items(),
+            ),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    os.path.join(launch_dir, "navigation_launch.py")
+                ),
+                launch_arguments={
+                    "namespace": namespace,
+                    "use_sim_time": use_sim_time,
+                    "autostart": autostart,
+                    "params_file": params_file,
+                    "use_composition": use_composition,
+                    "use_respawn": use_respawn,
+                    "container_name": "nav2_container",
+                }.items(),
+            ),
+            Node(
+                package="rviz2",
+                executable="rviz2",
+                name="rviz2",
+                arguments=["-d", rviz_config_dir],
+                parameters=[{"use_sim_time": use_sim_time}],
+                output="screen",
+            ),
+        ]
+    )
+
+    # Create the launch description and populate
+    ld = LaunchDescription()
+
+    # Set environment variables
+    ld.add_action(stdout_linebuf_envvar)
+
+    # Declare the launch options
+    ld.add_action(declare_namespace_cmd)
+    ld.add_action(declare_use_namespace_cmd)
+    ld.add_action(declare_map_yaml_cmd)
+    ld.add_action(declare_use_sim_time_cmd)
+    ld.add_action(declare_params_file_cmd)
+    ld.add_action(declare_autostart_cmd)
+    ld.add_action(declare_use_composition_cmd)
+    ld.add_action(declare_use_respawn_cmd)
+    ld.add_action(declare_log_level_cmd)
+
+    # Add the actions to launch all of the navigation nodes
+    ld.add_action(bringup_cmd_group)
+
+    return ld
